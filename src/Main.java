fc7a14ab9a639216276bb208cc82d3ad7b412bc9
yanuvelir <yanuvelir@gmail.com>
2024-04-17 14:38:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Main {
+
+    public  static void main(String[] args){
+
+    }
+
+    public static Zodiac_Signs signsRange(){
+        LocalDate currentDate = LocalDate.now();
+
+        Month month = currentDate.getMonth();
+        int monthValue = currentDate.getMonthValue();
+        String currentMonth = month.toString();
+
+        int dayOfMonth = currentDate.getDayOfMonth();
+
+        switch (monthValue){
+            case 12:
+                switch (dayOfMonth){
+                case 22: case 23: case 24:case 25: case 26: case 27: case 28: case 29: case 30: case 31:
+                        return Zodiac_Signs.CAPRICORN;
+                }
+            case 1:
+                switch (dayOfMonth){
+                    case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10:
+                    case 11: case 12: case 13: case 14: case 15: case 16: case 17: case 18: case 19:
+                        return Zodiac_Signs.CAPRICORN;
+                }
+
+        }
+
+    }
+}
